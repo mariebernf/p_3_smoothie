1e0a30623dbdf4ace7875f0626452c0cfac698dc
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-10 16:38:59 +0200

    Set up logout

--- smoothies/urls.py
+++ smoothies/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('update/<int:pk>/', views.smoothie_update, name='smoothie_update'),
     path('delete/<int:pk>/', views.smoothie_delete, name='smoothie_delete'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
