b6b4a429071f02c0103473327431c67c9febf858
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-14 00:29:54 +0200

    Update wsgi file, include whitenoise for static files

--- main/wsgi.py
+++ main/wsgi.py
@@ -4,3 +4,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
